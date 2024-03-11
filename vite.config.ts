4a19b47f5f38507cf51b189b7ee86e937b711734
vIad17 <75485055+vIad17@users.noreply.github.com>
2024-03-11 19:55:59 +0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 import svgr from 'vite-plugin-svgr';
 
 export default defineConfig(() => ({
+  base: '/webSequencer',
   plugins: [react(), tsconfigPaths(), svgr()],
   test: {
     exclude: [],
