c93fc5c0254630a5eef9ed7cc47a275b267cc9fc
VVAD <venya_kozlov_00@mail.ru>
2025-10-09 16:58:34 +0600

    [*] settings__range updated style

--- src/components/SoundSettings/RangeSetting.tsx
+++ src/components/SoundSettings/RangeSetting.tsx
@@ -31,16 +31,19 @@ const RangeSetting = ({
     <li className={clsx("sound-settings__element", className)}>
       {!hideText && <p className="sound-settings__name">{label}</p>}
       <label className="sound-settings__label" htmlFor={label}>
-        <input
-          id={label}
-          className="sound-settings__range"
-          type="range"
-          min={min}
-          max={max}
-          step={step}
-          defaultValue={value}
-          onChange={(event) => dispatch(setValue(event.target.valueAsNumber))}
-        />
+        <div className='sound-settings__range_bg' style={{ '--_settings-range-value': `${((value - min) / (max - min)) * 100}%` }}>
+          <input
+            id={label}
+            className="sound-settings__range"
+            type="range"
+            min={min}
+            max={max}
+            step={step}
+            defaultValue={value}
+            onChange={(event) => dispatch(setValue(event.target.valueAsNumber))}
+          />
+        </div>
+        
         {!hideText && <p className="sound-settings__range-value">{value}</p>}
       </label>
     </li>
