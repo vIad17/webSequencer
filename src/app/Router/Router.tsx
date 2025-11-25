f83625f4d9b7dd8a588307e16f2875fe0086d1cd
Muslim Kozhasov <muslim200411@gmail.com>
2025-11-25 14:25:56 +0600

    chore: remove comment,trash env variables and coping non-exist yarn.lock

--- src/app/Router/Router.tsx
+++ src/app/Router/Router.tsx
@@ -1,23 +1,21 @@
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
-import CreationField from 'src/features/DrawableField/components/CreationField/CreationField';
-import DrawableField from 'src/features/DrawableField/DrawableField';
 
 import InstrumentLayout from 'src/layouts/InstrumentLayout/InstrumentLayout';
+import Layout from 'src/layouts/Layout/Layout';
 
 import InstrumentPage from 'src/pages/InstrumentPage/InstrumentPage';
 import Main from 'src/pages/Main/OldMain';
-import Layout from 'src/layouts/Layout/Layout';
 import PreviewPage from 'src/pages/PreviewPage/PreviewPage';
 
 const Router = () => (
-  <BrowserRouter basename="/webSequencer">
+  <BrowserRouter>
     <Routes>
       <Route path="" element={<Layout />}>
         <Route path="preview" element={<PreviewPage />}></Route>
         <Route path="" element={<InstrumentLayout />}>
           <Route path="old" element={<Main />} />
           <Route path="" element={<InstrumentPage />} />
-          <Route path="*" element={<Navigate to="/webSequencer" />} />
+          <Route path="*" element={<Navigate to="/" />} />
         </Route>
       </Route>
     </Routes>
